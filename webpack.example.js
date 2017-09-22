6be0777bc3cd7ed83810a926aec8552a3552ff98
Changwan Jun <im@wani.kr>
2017-09-22 16:15:24 +0900

    add babel-es2015-ie presets for IE9

--- webpack.example.js
+++ webpack.example.js
@@ -23,7 +23,10 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015-ie'],  
+        },
       },
     ]
   },
