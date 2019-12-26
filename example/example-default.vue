430b554a2340e429f494ba7b3743255bb4bbe017
Changwan Jun <wan2land@gmail.com>
2019-12-26 14:08:42 +0900

    typo

--- example/example-default.vue
+++ example/example-default.vue
@@ -59,14 +59,16 @@ export default {
 <script>
 export default {
   data() {
-    data: null,
-    options: [
-      {value: "instagram", name: "<i class=\\"fab fa-instagram\\"></i> Instagram"},
-      {value: "facebook", name: "<i class=\\"fab fa-facebook\\"></i> Facebook"},
-      {value: "youtube", name: "<i class=\\"fab fa-youtube\\"></i> Youtube"},
-      {value: "twitter", name: "<i class=\\"fab fa-twitter\\"></i> Twitter"},
-      {value: "line", name: "<i class=\\"fab fa-line\\"></i> Line"},
-    ],
+    return {
+      data: null,
+      options: [
+        {value: "instagram", name: "<i class=\\"fab fa-instagram\\"></i> Instagram"},
+        {value: "facebook", name: "<i class=\\"fab fa-facebook\\"></i> Facebook"},
+        {value: "youtube", name: "<i class=\\"fab fa-youtube\\"></i> Youtube"},
+        {value: "twitter", name: "<i class=\\"fab fa-twitter\\"></i> Twitter"},
+        {value: "line", name: "<i class=\\"fab fa-line\\"></i> Line"},
+      ],
+    }
   }
 }
 <` + `/script>`,
