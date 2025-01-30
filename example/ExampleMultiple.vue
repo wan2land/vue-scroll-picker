82f4cf0fb9810e571127c0b4a867e06d6937b0c0
Changwan Jun <hey@wan2.land>
2025-01-30 17:14:28 +0900

    fix: example timer

--- example/ExampleMultiple.vue
+++ example/ExampleMultiple.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
 import {
-  VueScrollPicker,
-  VueScrollPickerOption,
-  VueScrollPickerValue,
-} from 'vue-scroll-picker'
-import { format, setDate, setMonth, setYear } from 'date-fns'
-
-defineProps<{
-  options: VueScrollPickerOption[]
-}>()
+  format,
+  setDate,
+  setHours,
+  setMinutes,
+  setMonth,
+  setSeconds,
+  setYear,
+} from 'date-fns'
+import { computed, ref } from 'vue'
+import { VueScrollPicker, VueScrollPickerValue } from 'vue-scroll-picker'
 
 const currentValue = ref(new Date())
 
 const currentYear = computed(() => currentValue.value.getFullYear())
 const currentMonth = computed(() => currentValue.value.getMonth() + 1)
 const currentDay = computed(() => currentValue.value.getDate())
+const currentAm = computed(() =>
+  currentValue.value.getHours() < 12 ? 'AM' : 'PM',
+)
+const currentHour = computed(() => currentValue.value.getHours() % 12)
+const currentMinute = computed(() => currentValue.value.getMinutes())
+const currentSecond = computed(() => currentValue.value.getSeconds())
 
 const years = computed(() => {
   const currYear = new Date().getFullYear()
@@ -25,13 +31,16 @@ const years = computed(() => {
     (_, index) => lastYear + index,
   ).reverse()
 })
-const months = computed(() =>
-  Array.from({ length: 12 }, (_, index) => index + 1),
-)
+const months = Array.from({ length: 12 }, (_, index) => index + 1)
 const days = computed(() => {
   const lastDay = new Date(currentYear.value, currentMonth.value, 0).getDate()
   return Array.from({ length: lastDay }, (_, index) => index + 1)
 })
+
+const amPm = ['AM', 'PM']
+const hours = Array.from({ length: 12 }, (_, index) => index)
+const minutes = Array.from({ length: 60 }, (_, index) => index)
+const seconds = Array.from({ length: 60 }, (_, index) => index)
 
 function handleUpdateYear(value: VueScrollPickerValue | undefined) {
   currentValue.value = setYear(currentValue.value, value as number)
@@ -44,12 +53,31 @@ function handleUpdateMonth(value: VueScrollPickerValue | undefined) {
 function handleUpdateDay(value: VueScrollPickerValue | undefined) {
   currentValue.value = setDate(currentValue.value, value as number)
 }
+
+function handleUpdateAm(value: VueScrollPickerValue | undefined) {
+  currentValue.value = setHours(currentValue.value, value === 'AM' ? 0 : 12)
+}
+
+function handleUpdateHour(value: VueScrollPickerValue | undefined) {
+  currentValue.value = setHours(
+    currentValue.value,
+    (value as number) + (currentAm.value === 'AM' ? 0 : 12),
+  )
+}
+
+function handleUpdateMinute(value: VueScrollPickerValue | undefined) {
+  currentValue.value = setMinutes(currentValue.value, value as number)
+}
+
+function handleUpdateSecond(value: VueScrollPickerValue | undefined) {
+  currentValue.value = setSeconds(currentValue.value, value as number)
+}
 </script>
 <template>
   <div>
     <div>
       Date =
-      <code>{{ format(currentValue, 'yyyy-MM-dd') }}</code>
+      <code>{{ format(currentValue, 'yyyy-MM-dd HH:mm:ss') }}</code>
     </div>
     <div class="picker-group">
       <VueScrollPicker
@@ -66,6 +94,26 @@ function handleUpdateDay(value: VueScrollPickerValue | undefined) {
         :options="days"
         :model-value="currentDay"
         @update:model-value="handleUpdateDay"
+      />
+      <VueScrollPicker
+        :options="amPm"
+        :model-value="currentAm"
+        @update:model-value="handleUpdateAm"
+      />
+      <VueScrollPicker
+        :options="hours"
+        :model-value="currentHour"
+        @update:model-value="handleUpdateHour"
+      />
+      <VueScrollPicker
+        :options="minutes"
+        :model-value="currentMinute"
+        @update:model-value="handleUpdateMinute"
+      />
+      <VueScrollPicker
+        :options="seconds"
+        :model-value="currentSecond"
+        @update:model-value="handleUpdateSecond"
       />
     </div>
   </div>
